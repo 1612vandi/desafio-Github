ce424ade926fd8658272bb9fe436a2f61b3c3d2d
vandi1612 <vandidanielmartinsfrancisco@gmail.com>
2022-12-22 22:56:16 +0000

    Treinando identação

---
+++ BoletimEstudantil/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+        if (mediaFinal < 6)
+            System.out.println("Aluno Reprovado");
+        else if (mediaFinal == 6) {
+            System.out.println("Prova Minerva");
+        }
+        else
+            System.out.println("Aprovado");
+    }
+}
